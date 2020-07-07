c64bcc48073c539fdc1ce6492f2237c888eac12c
EdsonMelgarejo <edsonmelgarejo97@gmail.com>
2020-07-07 14:41:47 -0500

    creada lista_item.xml, el adaptador de la lista de canciones y la Clase Cancion.kt
    Signed-off-by: Edson M <edsonmelgarejo97@gmail.com>

--- app/src/main/java/com/example/myapplication/ListaReproduccionActivity.kt
+++ app/src/main/java/com/example/myapplication/ListaReproduccionActivity.kt
@@ -1,11 +1,38 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_lista_reproduccion.*
 
 class ListaReproduccionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_reproduccion)
+
+        getCanciones()
+
+        btnReproducir.setOnClickListener{
+            val intent:Intent = Intent (this, ReproductorActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    fun getCanciones(){
+        val cancion1 = Cancion("Cancion1", "Album1", R.drawable.live_streaming)
+        val cancion2 = Cancion("Cancion2", "Album1", R.drawable.live_streaming)
+        val cancion3 = Cancion("Cancion3", "Album2", R.drawable.live_streaming)
+        val cancion4 = Cancion("Cancion4", "Album2", R.drawable.live_streaming)
+        val cancion5 = Cancion("Cancion5", "Album3", R.drawable.live_streaming)
+
+        val listaCanciones = listOf(cancion1, cancion2, cancion3, cancion4, cancion5)
+
+        val adapter = Cancion_Adapter(this, listaCanciones)
+
+       listCanciones.adapter = adapter
+    }
+
+    fun reproducir(){
+
     }
 }
